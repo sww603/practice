02569643c155e7123dc925ffac6df4b691cc7cbc
sww <sww_603@163.com>
2019-05-20 18:38:46 +0800

    更新

--- src/ThreadPool.java
+++ src/ThreadPool.java
@@ -1,6 +1,7 @@
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by sww_6 on 2019/5/20.
@@ -8,18 +9,53 @@ import java.util.concurrent.Executors;
 public class ThreadPool {
 
   public static void main(String[] args) {
-    ExecutorService service = Executors.newFixedThreadPool(5);
 
-    for (int i = 0; i < 10; i++) {
-      try {
-        service.submit(() -> {
-          System.out.println(Thread.currentThread().getName() + "\t");
+    /*//第一种，固定的线程数
+    ExecutorService service = Executors.newFixedThreadPool(5);
+    Future<Integer> result = null;
+    try {
+      for (int i = 0; i < 10; i++) {
+        result = service.submit(() -> {
+          System.out.println(Thread.currentThread().getName());
           return new Random().nextInt(20);
         });
-      } catch (Exception e) {
-        e.printStackTrace();
-      } finally {
+        System.out.println("result" + result.get());
       }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+    }*/
+
+   /* //第二种 只有一个线程
+    ExecutorService service = Executors.newSingleThreadExecutor();
+    Future<Integer> result = null;
+    try {
+      for (int i = 0; i < 10; i++) {
+        result = service.submit(() -> {
+          System.out.println(Thread.currentThread().getName());
+          return new Random().nextInt(20);
+        });
+        System.out.println(result.get());
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+    }*/
+
+    //第三种 可改变线程
+    ExecutorService service = Executors.newCachedThreadPool();
+    Future<Integer> result = null;
+    try {
+      for (int i = 0; i < 1000; i++) {
+        result = service.submit(() -> {
+          System.out.println(Thread.currentThread().getName());
+          return new Random().nextInt(20);
+        });
+        System.out.println(result.get());
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
     }
   }
 }
